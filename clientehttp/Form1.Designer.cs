7a7df794782faa111097a9e9e8c27164de4a3d6f
Carlos Melo <carlaofernandesedu@gmail.com>
2015-08-17 16:26:13 -0300

    Subindo projeto windowform teste client webapi

---
+++ clientehttp/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace clientehttp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTimeout = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.chkexibir = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtexecucoes = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.lblfinalizado = new System.Windows.Forms.Label();
+            this.lblproxy = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(180, 12);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(585, 20);
+            this.txturl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TIMEOUT(ms)";
+            // 
+            // txtTimeout
+            // 
+            this.txtTimeout.Location = new System.Drawing.Point(214, 151);
+            this.txtTimeout.Name = "txtTimeout";
+            this.txtTimeout.Size = new System.Drawing.Size(137, 20);
+            this.txtTimeout.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(31, 270);
+            this.txtresultado.Multiline = true;
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtresultado.Size = new System.Drawing.Size(911, 244);
+            this.txtresultado.TabIndex = 9;
+            // 
+            // chkexibir
+            // 
+            this.chkexibir.AutoSize = true;
+            this.chkexibir.Location = new System.Drawing.Point(380, 152);
+            this.chkexibir.Name = "chkexibir";
+            this.chkexibir.Size = new System.Drawing.Size(121, 17);
+            this.chkexibir.TabIndex = 10;
+            this.chkexibir.Text = "Exibir conteudo html";
+            this.chkexibir.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtexecucoes);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtvalor);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(126, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(550, 48);
+            this.panel1.TabIndex = 11;
+            // 
+            // txtexecucoes
+            // 
+            this.txtexecucoes.Location = new System.Drawing.Point(381, 16);
+            this.txtexecucoes.Name = "txtexecucoes";
+            this.txtexecucoes.Size = new System.Drawing.Size(68, 20);
+            this.txtexecucoes.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Execucoes";
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(89, 15);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(209, 20);
+            this.txtvalor.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Authorization";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(128, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 41);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tipo de Carga";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(109, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Arquivo";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tela";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(31, 236);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 13);
+            this.lblresult.TabIndex = 13;
+            // 
+            // lblfinalizado
+            // 
+            this.lblfinalizado.AutoSize = true;
+            this.lblfinalizado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfinalizado.Location = new System.Drawing.Point(35, 521);
+            this.lblfinalizado.Name = "lblfinalizado";
+            this.lblfinalizado.Size = new System.Drawing.Size(52, 18);
+            this.lblfinalizado.TabIndex = 14;
+            this.lblfinalizado.Text = "label5";
+            // 
+            // lblproxy
+            // 
+            this.lblproxy.AutoSize = true;
+            this.lblproxy.Location = new System.Drawing.Point(538, 155);
+            this.lblproxy.Name = "lblproxy";
+            this.lblproxy.Size = new System.Drawing.Size(33, 13);
+            this.lblproxy.TabIndex = 15;
+            this.lblproxy.Text = "Proxy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 558);
+            this.Controls.Add(this.lblproxy);
+            this.Controls.Add(this.lblfinalizado);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.chkexibir);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtTimeout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Envio de Dados - Web API (Simultaneo)  NET 4.0  v 1.4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTimeout;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.CheckBox chkexibir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtexecucoes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.Label lblfinalizado;
+        private System.Windows.Forms.Label lblproxy;
+    }
+}
+
---
+++ clientehttp/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Cache;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace clientehttp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _timeoutconexao;
+        private string _urlbase;
+        private string _authorization;
+        private string _intervaloentreexecucoes;
+        private string _caminhoarquivos;
+        private string _proxy;
+        private string _cache;
+
+        private void CarregarValoresIniciais()
+        {
+            _proxy = ConfigurationManager.AppSettings["proxy"];
+            _cache = ConfigurationManager.AppSettings["cache"];
+            _timeoutconexao = ConfigurationManager.AppSettings["timeoutconexao"];
+            _urlbase = ConfigurationManager.AppSettings["urlbase"];
+            _authorization = ConfigurationManager.AppSettings["authorization"];
+            _intervaloentreexecucoes = ConfigurationManager.AppSettings["intervaloentreexecucoes"];
+            _caminhoarquivos = ConfigurationManager.AppSettings["caminhoarquivos"] == "[local]" ? Application.StartupPath : ConfigurationManager.AppSettings["caminhoarquivos"];
+
+
+        }
+
+        private List<string> CarregarArquivo(string caminho)
+        {
+            List<string> valores = new List<string>();
+            string fileName = string.Empty;
+            try
+            {
+                fileName = caminho;
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string s = String.Empty;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        valores.Add(s);
+                    }
+                }
+                return valores;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao ler arquivo " + fileName + " |" + ex.StackTrace);
+            }
+        }
+
+        private void ExecutarHashDigitado()
+        {
+            button1.Enabled = false;
+            List<string> valores = new List<string>();
+            int execucoes = Convert.ToInt32(txtexecucoes.Text);
+            for (int i = 0; i < execucoes; i++)
+                valores.Add(txtvalor.Text);
+            txtresultado.Clear();
+            lblresult.Text = "";
+            Application.DoEvents();
+            var url = txturl.Text;
+            var timeout = Convert.ToInt32(txtTimeout.Text);
+            var exibir = chkexibir.Checked;
+            StringBuilder sb = new StringBuilder();
+            Parallel.For(0, valores.Count, i =>
+            {
+                sb.Append(RequisitarDados(i + 1, url, valores[i], timeout, exibir, _proxy,_cache));
+            });
+            txtresultado.Text = sb.ToString();
+            button1.Enabled = true;
+        }
+
+        private FileInfo[] CarregarNomesArquivos(string _diretorio)
+        {
+            if (Directory.Exists(_diretorio))
+            {
+                DirectoryInfo diretorio = new DirectoryInfo(_diretorio);
+                FileInfo[] Arquivos = diretorio.GetFiles("*.txt");
+                var ArquivosOrdenados = Arquivos.OrderBy(x => x.Name).ToArray();
+                return ArquivosOrdenados;
+            }
+            else
+            {
+                throw new Exception("Não foi possível ler o diretório " + _diretorio);
+            }
+        }
+
+        private bool SalvarExecucaoEmArquivo(string conteudo)
+        {
+            var bret = false;
+            try
+            {
+                var nomearquivo = "execucao_" + DateTime.Now.ToString("ddMMyyyy_HHmmss");
+                nomearquivo = Path.Combine(Application.StartupPath, nomearquivo);
+                var encode = System.Text.Encoding.GetEncoding("iso-8859-8");
+                using (StreamWriter sw = new StreamWriter(nomearquivo, false, encode))
+                {
+                    sw.Write(conteudo);
+                    bret = true;
+                }
+
+            }
+            catch { }
+            return bret;
+
+        }
+
+        private void ExecutarHashDeArquivos()
+        {
+            button1.Enabled = false;
+            var oFiles = CarregarNomesArquivos(_caminhoarquivos);
+            List<string> valores = new List<string>();
+            var url = txturl.Text;
+            var timeout = Convert.ToInt32(txtTimeout.Text);
+            var exibir = chkexibir.Checked;
+            StringBuilder sb = new StringBuilder();
+            foreach (var ofile in oFiles)
+            {
+                var caminho = ofile.FullName;
+                valores = CarregarArquivo(caminho);
+                txtresultado.Clear();
+                sb.Clear();
+                lblresult.Text = "Envio dos Hashs do arquivo:" + caminho;
+                Application.DoEvents();
+                Parallel.For(0, valores.Count, i =>
+                {
+                    sb.Append(RequisitarDados(i + 1, url, valores[i], timeout, exibir, _proxy,_cache));
+                });
+                txtresultado.Text = sb.ToString();
+                if (SalvarExecucaoEmArquivo(sb.ToString()))
+                    lblresult.Text += "\nInformação registrada também em arquivo no diretório " + Application.StartupPath;
+
+                Application.DoEvents();
+
+                var intervalo = Convert.ToInt32(_intervaloentreexecucoes);
+
+                if ((oFiles.Count() > 1) && intervalo > 0)
+                    Thread.Sleep(intervalo);
+            }
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                lblfinalizado.Text = "";
+                if (radioButton1.Checked)
+                {
+                    ExecutarHashDigitado();
+                }
+                else
+                {
+                    ExecutarHashDeArquivos();
+                }
+                lblfinalizado.Text = "Processo finalizado";
+            }
+            catch (Exception ex)
+            {
+                lblfinalizado.Text = "Ocorreu um um erro na execução! Erro: " + ex.Message;
+                button1.Enabled = true;
+            }
+
+        }
+
+        
+        private static IWebProxy ObterProxy(string nomeproxy)
+        {
+            IWebProxy proxy = null;
+            try
+            {
+                if (nomeproxy == "[local]")
+                {
+                    
+                    var ieproxy = WebRequest.GetSystemWebProxy();
+                    if (ieproxy != null && ((WebProxy)ieproxy).Address.AbsoluteUri != String.Empty)
+                    {
+                        proxy  = ieproxy;
+                    }
+                }
+                else
+                {
+                    proxy = new WebProxy(nomeproxy);
+                }
+            }
+            catch {}
+            return proxy;
+        }
+
+        private static string RequisitarDados(int execucoes, string url, string valor, int timeout, bool exibir, string proxy,string cache)
+        {
+            StringBuilder retorno = new StringBuilder();
+            WebResponse myWebResponse = null;
+            DateTime horainicial;
+            DateTime horafinal;
+            try
+            {
+                var uri = new Uri(url);
+                HttpWebRequest myWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+                if (!String.IsNullOrEmpty(proxy))
+                {
+                    var userproxy = ObterProxy(proxy);
+                    if (userproxy != null)
+                    {
+                        //userproxy.UseDefaultCredentials = true;
+                        myWebRequest.Proxy = userproxy;
+                    }
+                }
+                if (cache == "disabled")
+                {
+                  var policy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
+                  WebRequest.DefaultCachePolicy = policy;
+                }
+                myWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36";
+                myWebRequest.KeepAlive = true;
+
+
+                if (!String.IsNullOrEmpty(valor))
+                    myWebRequest.Headers[HttpRequestHeader.Authorization] = valor;
+
+                myWebRequest.Timeout = timeout;
+                myWebRequest.Accept = "*/*";
+                myWebRequest.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate, sdch";
+                myWebRequest.Headers[HttpRequestHeader.AcceptLanguage] = "pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4";
+                myWebRequest.Headers[HttpRequestHeader.Pragma] = "no-cache";
+
+                horainicial = DateTime.Now;
+                retorno.AppendLine("hora;" + horainicial.ToString("dd/MM/yyyy HH:mm:ss") + ";hash;" + valor + ";status;;descr;;ENV;" + execucoes.ToString() + ";");
+                //retorno.AppendLine("Hora:" + horainicial.ToString("dd/MM/yyyy HH:mm:ss") + " Enviando execucao:" + execucoes.ToString() + " url:" + url + " hash:" + valor);
+                // Send the 'WebRequest' and wait for response.
+                myWebResponse = myWebRequest.GetResponse();
+
+                var rethttp = (HttpWebResponse)myWebResponse;
+                int codhttp = (int)rethttp.StatusCode;
+
+                horafinal = DateTime.Now;
+                retorno.AppendLine("hora;" + horafinal.ToString("dd/MM/yyyy HH:mm:ss") + ";hash;" + valor + ";status;" + codhttp.ToString() + ";descr;" + rethttp.StatusDescription + ";RET;" + execucoes.ToString() + ";body;" + myWebResponse.ResponseUri + ";");
+                //retorno.AppendLine("Hora:" + horafinal.ToString("dd/MM/yyyy HH:mm:ss")+ " Recebendo dados URL:" + myWebResponse.ResponseUri  + " Status:" + codhttp.ToString() + "-" + rethttp.StatusDescription);
+
+                // Obtain a 'Stream' object associated with the response object.
+                Stream ReceiveStream = myWebResponse.GetResponseStream();
+
+                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+
+                // Pipe the stream to a higher level stream reader with the required encoding format. 
+                using (StreamReader readStream = new StreamReader(ReceiveStream, encode))
+                {
+                    if (exibir)
+                        retorno.AppendLine(readStream.ReadToEnd());
+
+                    readStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                horafinal = DateTime.Now;
+                var descricao = ex.Message;
+                var codhttp = "";
+                var lista = new string[] { "400", "401", "402", "403", "404", "405", "500", "501", "502", "503", "504", "505" };
+                foreach (var item in lista)
+                {
+                    if (descricao.Contains(item))
+                    {
+                        codhttp = item;
+                        break;
+                    }
+
+                }
+
+                retorno.AppendLine("hora;" + horafinal.ToString("dd/MM/yyyy HH:mm:ss") + ";hash;" + valor + ";status;" + codhttp + ";descr;" + descricao + ";RET;" + execucoes.ToString() + ";");
+                //retorno.AppendLine("Hora:" + horafinal.ToString("dd/MM/yyyy HH:mm:ss") + " Erro:" + ex.Message);
+
+            }
+            finally
+            {
+                // Release the resources of response object.
+                if (myWebResponse != null)
+                    myWebResponse.Close();
+
+
+            }
+
+            return retorno.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarValoresIniciais();
+            lblfinalizado.Text = "";
+            txtexecucoes.Text = "1";
+            txturl.Text = _urlbase;
+            txtTimeout.Text = _timeoutconexao;
+            txtvalor.Text = _authorization;
+            radioButton1.Checked = true;
+            lblproxy.Text = "Info Proxy:Usando a configuração padrão";
+            if (_proxy != String.Empty)
+            {
+                var oproxy = ObterProxy(_proxy);
+                lblproxy.Text = "Info Proxy:";
+                if (oproxy != null)
+                {
+                    var userproxy = (WebProxy) oproxy; 
+                    lblproxy.Text += userproxy.Address.AbsoluteUri != String.Empty ? userproxy.Address.AbsoluteUri : "Configuracao do browser sem proxy";
+                    lblproxy.Text += " Usa Credenciais" + userproxy.UseDefaultCredentials.ToString(); 
+                }
+            }
+
+        }
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                panel1.Visible = true;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                panel1.Visible = false;
+            }
+        }
+    }
+}
